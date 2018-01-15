9a1d39743694eec53da422f99ac7f463b4fd27b8
rpappa <rjpappa@gmail.com>
2018-01-15 16:14:06 -0500

    update cube vision

--- 340-Vision/cubes.py
+++ 340-Vision/cubes.py
@@ -8,6 +8,8 @@ import cv2
 import numpy as np
 import math
 
+import socket
+
 # camera setup
 
 camera = PiCamera(resolution="320x240", framerate = 60) # use 60 fps to avoid issues with rolling shutter
@@ -19,7 +21,14 @@ nt.initialize(server='roborio-424-frc.local')
 
 time.sleep(5) # camera warmup, position setup
 
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5803
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
 startTime = time.time(); # for timing purposes
+
 
 # main execution
 
@@ -38,27 +47,34 @@ for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # hsv thresholds
-    lower = np.array([18,100,89])
+    lower = np.array([21,100,89])
     upper = np.array([39,255,255])
 
     thresh = cv2.inRange(hsv, lower, upper)
     # find contours
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    centerx = 0
+    centerx = -1
     centery = 0
+    width = -1
     contour = -1
+
+    # sock.sendto(str(160),   ("roborio-424-frc.local", 5803))
 
     # filter contours
     for i in range(0, len(contours)):
-         if cv2.contourArea(contours[i]) > 500:
+         if cv2.contourArea(contours[i]) > 1500:
              x,y,w,h = cv2.boundingRect(contours[i])
+             width = w
              centerx = x + w/2 # info about the target
              centery = y + h/2 # info about the target
              contour = i
-             vi.putNumber('centerx', centerx);
-             print(centerx)
 
+
+    print(centerx)
+    vi.putNumber('centerx', centerx);
+    vi.putNumber('width', width);
+    sock.sendto(str(centerx) + "," + str(width),   ("roborio-424-frc.local", 5803))
     # save debug images
     cv2.imwrite("cube_orig.jpg", frame)
 
